6dbb936284b2923a50a9ad4665f318bd8f78e931
Pablo Torrez <pablotorrez90@outlook.com>
2023-11-29 09:23:59 -0300

    modif

--- interval.js
+++ interval.js
@@ -1,19 +1,35 @@
 const tiempo = document.getElementById("limite");
 const mostrarTexto = document.getElementById("txt-res");
+let btn = document.getElementById("btn-empezar");
 
+let intervalo = null; // para iniciar y terminar el intervalo
 let contador = 10;
 
-const time = setInterval(() => {
-    tiempo.innerText = contador;
-    contador--;
+btn.addEventListener("click", function () {
+    intervalo = setInterval(() => {
+        tiempo.innerText = contador;
+        contador--;
 
-    if (contador <= 0) {
-        clearTimeout(time);
-        mostrarTexto.innerHTML = `
+        if (contador < 0) {
+            clearInterval(intervalo);
+            mostrarTexto.innerHTML = `
             <h1 class="title-timeout">Se acabó el tiempo!</h1>
         `
-    }
-}, 1000);
+        }
+    }, 1000);
+});
+
+function coloresAleatorios() {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+function cambiarColorTexto() {
+    const txt = document.getElementById("txt-res");
+    txt.style.color = coloresAleatorios();
+}
+
+setInterval(cambiarColorTexto, 1000);
+
 
 
 
